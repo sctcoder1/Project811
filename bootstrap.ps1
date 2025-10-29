e82eefc9e30a5a0f5581819144b9a3ab7f7e2a94
sctcoder1 <bdooley@courts.ms.gov>
2025-10-29 13:58:52 -0500

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -34,77 +34,17 @@ try {
     Log "Unblocked PowerShell scripts in $Root."
 } catch { Log "Warning: Could not unblock files. $_" }
 
-# Remove previous task if it exists
+# --- Run StageUpgrade task ---
 try {
-    if (Get-ScheduledTask -TaskName $Task -ErrorAction SilentlyContinue) {
-        Unregister-ScheduledTask -TaskName $Task -Confirm:$false -ErrorAction SilentlyContinue
-        Log "Removed old $Task task."
-    }
-} catch { Log "Warning: Could not remove old task. $_" }
-
-# --- Create the scheduled task XML ---
-try {
-    if (Test-Path $StageScript) {
-        Log "Found stage-upgrade.ps1 at $StageScript"
-
-        $StageDir = Split-Path $StageScript -Parent
-        Log "Using working directory: $StageDir"
-
-        # Build XML content dynamically
-        $XMLContent = @"
-<?xml version="1.0" encoding="UTF-16"?>
-<Task version="1.4" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
-  <Triggers>
-    <TimeTrigger>
-      <StartBoundary>$((Get-Date).AddSeconds(30).ToString('yyyy-MM-ddTHH:mm:ss'))</StartBoundary>
-      <Enabled>true</Enabled>
-    </TimeTrigger>
-  </Triggers>
-  <Principals>
-    <Principal id="Author">
-      <UserId>SYSTEM</UserId>
-      <RunLevel>HighestAvailable</RunLevel>
-    </Principal>
-  </Principals>
-  <Settings>
-    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
-    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
-    <StopIfGoingOnBatteries>false</StopIfGoingOnBatteries>
-    <AllowHardTerminate>true</AllowHardTerminate>
-    <StartWhenAvailable>true</StartWhenAvailable>
-    <AllowStartOnDemand>true</AllowStartOnDemand>
-    <Enabled>true</Enabled>
-    <Hidden>false</Hidden>
-    <ExecutionTimeLimit>PT0S</ExecutionTimeLimit>
-    <UseUnifiedSchedulingEngine>true</UseUnifiedSchedulingEngine>
-  </Settings>
-  <Actions Context="Author">
-    <Exec>
-      <Command>$PsExe</Command>
-      <Arguments>-ExecutionPolicy Bypass -NoProfile -File `"$StageScript`"</Arguments>
-      <WorkingDirectory>$StageDir</WorkingDirectory>
-    </Exec>
-  </Actions>
-</Task>
-"@
-
-        $XMLContent | Out-File -Encoding Unicode -FilePath $XmlPath -Force
-        Log "Created task XML at $XmlPath"
-
-        # Register and run the task
-        $Create = schtasks /create /tn $Task /xml $XmlPath /ru SYSTEM /f 2>&1
-        Log "schtasks /create output: $Create"
-
-        $Run = schtasks /run /tn $Task 2>&1
-        Log "schtasks /run output: $Run"
-
-        Log "Task $Task registered and started successfully via XML."
+    if (Get-ScheduledTask -TaskName "Win11_StageUpgrade" -ErrorAction SilentlyContinue) {
+        schtasks /run /tn "Win11_StageUpgrade" | Out-Null
+        Log "Ran existing StageUpgrade task."
     } else {
-        Log "ERROR: stage-upgrade.ps1 not found at $StageScript"
+        Log "ERROR: StageUpgrade task not found."
     }
 }
 catch {
-    Log "FATAL during task creation: $_"
+    Log "FATAL: could not start StageUpgrade task. $_"
 }
 
 Start-Sleep -Seconds 5
